b80d63a48f618f5808aa9873e672eae670b23f04
Talha668 <132550831+Talha668@users.noreply.github.com>
2025-08-13 19:08:20 +0500

    Add files via upload

---
+++ Stopwatch/Main.py
@@ -0,0 +1,81 @@
+# StopWatch
+
+import sys
+from PyQt5.QtWidgets import (QApplication, QPushButton, QWidget, QLabel, QVBoxLayout, QHBoxLayout)
+from PyQt5.QtCore import QTimer, QTime, Qt
+
+class Stopwatch(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.time = QTime(0, 0, 0, 0)
+        self.time_label = QLabel("00:00:00:00", self)
+        self.start_button = QPushButton("Start", self)
+        self.stop_button = QPushButton("Stop", self)
+        self.reset_button = QPushButton("Reset", self)
+        self.timer = QTimer(self)
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("StopWatch")
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.time_label)
+
+        self.time_label.setAlignment(Qt.AlignCenter)
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.start_button)
+        hbox.addWidget(self.stop_button)
+        hbox.addWidget(self.reset_button)
+
+        vbox.addLayout(hbox)
+        self.setLayout(vbox)
+
+        self.setStyleSheet("""
+            QPushButton, QLabel{
+               padding: 20px;
+               font-weight: bold;
+               font-family: Calibri;
+            }       
+            QPushButton{
+               font-size: 50px;
+            }
+            QLabel{
+               font-size: 120px;
+               Background-color: hsl(200, 100%, 85%);
+               Border-radius: 20px;
+            }        
+        """)
+        self.start_button.clicked.connect(self.start)
+        self.stop_button.clicked.connect(self.stop)
+        self.reset_button.clicked.connect(self.reset)
+        self.timer.timeout.connect(self.update_display)
+
+    def start(self):
+        self.timer.start(10)
+
+    def stop(self):
+        self.timer.stop()
+
+    def reset(self):
+        self.timer.stop()
+        self.time = QTime(0, 0, 0, 0)
+        self.time_label.setText(self.format_time(self.time))
+
+    def format_time(self, time):
+        hours = time.hour()
+        minutes = time.minute()
+        second = time.second()
+        millisecond = time.msec() // 10
+        return f"{hours:02}:{minutes:02}:{second:02}:{millisecond:02}"
+
+    def update_display(self):
+        self.time = self.time.addMSecs(10)
+        self.time_label.setText(self.format_time(self.time))
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    stopwatch = Stopwatch()
+    stopwatch.show()
+    sys.exit(app.exec_())
