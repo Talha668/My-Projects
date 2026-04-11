f836631fcee4029f1f4fa013b64620c395e30545
Talha668 <132550831+Talha668@users.noreply.github.com>
2026-04-11 20:01:37 +0500

    Delete To Do App/todo directory

--- To Do App/todo/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('update/<str:pk>/', views.updateTask, name = "update"),
-    path('delete/<str:pk>/', views.deleteTask, name= "delete"),
-]
